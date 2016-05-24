cbc42491eb3108021d60327ac20d8e3703dd938f
LungTakumi <lungtakumi@gmail.com>
2016-05-24 11:25:53 +0800

    testpdo

--- DBConnect.php
+++ DBConnect.php
@@ -1,10 +1,6 @@
 <?php #DBConnect.php
-$conn = new mysqli();
-$host ="postgres://niclzsjyoobfnu:86vVCQmpq47Z6893OZ5qwYdSx1@ec2-54-225-100-236.compute-1.amazonaws.com:5432/d2d2lsqfkquc0m";
-$user ="niclzsjyoobfnu";
-$password = "86vVCQmpq47Z6893OZ5qwYdSx1";
-$dbname = "d2d2lsqfkquc0m";
-$conn -> connect($host, $user, $password, $dbname);
+$conn = new PDO("pgsql:host=ec2-54-225-100-236.compute-1.amazonaws.com;port=5432;dbname=d2d2lsqfkquc0m;user=niclzsjyoobfnu;password=86vVCQmpq47Z6893OZ5qwYdSx1"); 
+
 if (mysqli_connect_errno()) {
  echo("Failed to connect, the error message is : ".
  mysqli_connect_error());
