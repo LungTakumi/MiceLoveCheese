c6f17929a0092bbf89390c2f361de82b42569db8
LungTakumi <lungtakumi@gmail.com>
2016-05-24 17:35:06 +0800

    debug

--- DBConnect.php
+++ DBConnect.php
@@ -6,6 +6,8 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
+ echo($server." ".$username." ".$password." ".$db);
+
 $conn = new mysqli($server, $username, $password, $db);
 
 if (mysqli_connect_errno()) {
