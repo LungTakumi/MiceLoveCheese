69cbe2af47e4337417d69ecfd0eb2373360256b7
LungTakumi <lungtakumi@gmail.com>
2016-05-24 15:34:30 +0800

    2

---
+++ DBConnect.php
@@ -0,0 +1,18 @@
+<?php #DBConnect.php
+$conn = new mysqli();
+$host ="ec2-54-225-100-236.compute-1.amazonaws.com";
+$user ="niclzsjyoobfnu";
+$password = "86vVCQmpq47Z6893OZ5qwYdSx1";
+$dbname = "d2d2lsqfkquc0m";
+$conn -> connect($host, $user, $password, $dbname);
+if (mysqli_connect_errno()) {
+ echo("Failed to connect, the error message is : ".
+ mysqli_connect_error());
+ exit();
+}
+
+if (!mysqli_set_charset($conn, "utf8")) {  
+} 
+else {
+} 
+?>
